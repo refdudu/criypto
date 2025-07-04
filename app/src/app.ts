cfa0df4502291af619a0a9863e1faf5021635bf5
Renan Fischer <renanfisher@atendare.com>
2025-07-04 18:59:41 -0300

    fix: update minVolume24h to 700,000 for USDT volume

--- app/src/app.ts
+++ app/src/app.ts
@@ -21,7 +21,7 @@ export const config = {
     enabled: true,
     // topNGainers: 100,
     quoteAsset: "USDT",
-    minVolume24h: 666_666, // 50 mil em volume de USDT
+    minVolume24h: 700_000, // 50 mil em volume de USDT
     fallbackSymbols: ["BTCUSDT", "ETHUSDT"], // Símbolos para monitorar se a busca dinâmica estiver desabilitada
   },
 //   intervals: ["5m", "15m", "1h", "4h"],
