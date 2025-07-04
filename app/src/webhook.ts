a495e9548d5b4f61522641448f0657bb39b1ab92
Renan Fischer <renanfisher@atendare.com>
2025-07-04 18:19:37 -0300

    refactor: simplify webhook function by removing response logging

--- app/src/webhook.ts
+++ app/src/webhook.ts
@@ -2,16 +2,13 @@ import "dotenv/config";
 
 export const webhook = async (body: object) => {
   try {
-    const response = await fetch(process.env.WEBHOOK_URL || "", {
+    await fetch(process.env.WEBHOOK_URL || "", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
-    console.log("Webhook response status:", response.status);
-    const data = await response.json();
-    console.log(data);
   } catch (error) {
     console.error("Error sending webhook:", error);
   }
