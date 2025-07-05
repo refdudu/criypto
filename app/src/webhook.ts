4da2188dfc57374d8ed49f9fd6f39adf303db611
Renan Fischer <renan.fischer.2013@gmail.com>
2025-07-04 23:38:52 -0300

    fix: corrigir a captura de resposta no envio de webhook

--- app/src/webhook.ts
+++ app/src/webhook.ts
@@ -2,16 +2,18 @@ import "dotenv/config";
 
 export const webhook = async (url: string, body: object) => {
   try {
-    await fetch(url, {
+    const response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
+    console.log(response.status, response.statusText);
   } catch (error) {
     console.error("Error sending webhook:", error);
   }
+  
 };
 
 export const lucaWebhook = async (body: {
