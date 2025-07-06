2ea1cbf1f6ea1707bea4a0cf2e474226dd6cd30a
Renan Fischer <renan.fischer.2013@gmail.com>
2025-07-05 23:53:35 -0300

    refactor: simplificar a fila de salvamento de Kline, removendo a função assíncrona desnecessária

--- app/src/queues/saveKlineQueue.ts
+++ app/src/queues/saveKlineQueue.ts
@@ -2,18 +2,15 @@ import { HistoricalKlineData } from "../interfaces";
 import { SupabaseCoinRepository } from "../supabase/SupabaseCoinRepository";
 import { QueueManager } from "./QueueManager";
 
-// Fila para salvar histórico
 interface SaveKlineQueueTask {
   eventSymbol: string;
   interval: string;
   newKlineData: HistoricalKlineData;
 }
-export const saveKlineQueue = new QueueManager(
-  async (task: SaveKlineQueueTask) => {
-    await SupabaseCoinRepository.saveSymbolIntervalData(
-      task.eventSymbol,
-      task.interval,
-      task.newKlineData
-    );
-  }
+export const saveKlineQueue = new QueueManager((task: SaveKlineQueueTask) =>
+  SupabaseCoinRepository.saveSymbolIntervalData(
+    task.eventSymbol,
+    task.interval,
+    task.newKlineData
+  ).then(() => console.log(`Salvo ${task.eventSymbol}@${task.interval}`))
 );
